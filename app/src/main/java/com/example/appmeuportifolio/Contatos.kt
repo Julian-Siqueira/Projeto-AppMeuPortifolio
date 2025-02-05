f83905a927ad8ad12241461c0d7d5e198cac4ddc
Julian-Siqueira <juliansiqueiradev@gmail.com>
2025-02-05 12:14:38 -0300

    Criado Toolbar e temas personalizados

--- app/src/main/java/com/example/appmeuportifolio/Contatos.kt
+++ app/src/main/java/com/example/appmeuportifolio/Contatos.kt
@@ -1,20 +1,26 @@
 package com.example.appmeuportifolio
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.appmeuportifolio.databinding.ActivityContatosBinding
+import com.example.appmeuportifolio.databinding.ActivityMainBinding
 
 class Contatos : AppCompatActivity() {
+
+    private lateinit var binding: ActivityContatosBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_contatos)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityContatosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionbarContato.setNavigationOnClickListener{
+            finish()
         }
+
     }
 }
